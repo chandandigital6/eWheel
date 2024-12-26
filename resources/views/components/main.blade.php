d70a6852d90fd650be64de859a0ea42c86c40813
kajal kumari <2005050100024kajal@gmail.com>
2024-12-26 13:22:32 +0530

    one

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -4,32 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Litho Power Battery</title>
-    <link rel="icon" type="image/png" href="{{ asset('asset/img/LITHO_POWER.png') }}" />
+    <title>E_Wheels</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    {{-- icons --}}
     <link
     href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css"
     rel="stylesheet"
 />
-{{-- slider --}}
-<link
-  crossorigin="anonymous"
-  href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/8.3.2/swiper-bundle.css"
-  rel="stylesheet"
-/>
-<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-
 </head>
-<body>
+<body class="bg-white">
     @include('components.header')
 
-    @yield('comtent')
+    <div class="mx-0">
+        @yield('content')
+    </div>
 
-    @include('components.footer')
-    <script
-  crossorigin="anonymous"
-  defer
-  src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/8.3.2/swiper-bundle.min.js"
-></script>
+    @include('components.footer')    
+
 </body>
 </html>
