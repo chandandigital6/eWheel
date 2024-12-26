850b7d5fd4360c188ac76bd2db1563f16b38b1dd
chandan <chandandigital6@gmail.com>
2024-12-26 13:25:08 +0530

    fix

--- routes/web.php
+++ routes/web.php
@@ -100,14 +100,14 @@ Route::group(['middleware' => ['auth']], function () {
 
 
 
-//product
-Route::get('product/index', [ProductController::class, 'index'])->name('product.index');
-Route::get('product/create', [ProductController::class, 'create'])->name('product.create');
-Route::post('product/store', [ProductController::class, 'store'])->name('product.store');
-Route::get('product/edit/{product}', [ProductController::class, 'edit'])->name('product.edit');
-Route::post('product/update/{product}', [ProductController::class, 'update'])->name('product.update');
-Route::get('product/delete/{product}', [ProductController::class, 'delete'])->name('product.delete');
-Route::get('product/duplicate/{product}', [ProductController::class, 'duplicate'])->name('product.duplicate');
+    //product
+    Route::get('product/index', [ProductController::class, 'index'])->name('product.index');
+    Route::get('product/create', [ProductController::class, 'create'])->name('product.create');
+    Route::post('product/store', [ProductController::class, 'store'])->name('product.store');
+    Route::get('product/edit/{product}', [ProductController::class, 'edit'])->name('product.edit');
+    Route::post('product/update/{product}', [ProductController::class, 'update'])->name('product.update');
+    Route::get('product/delete/{product}', [ProductController::class, 'delete'])->name('product.delete');
+    Route::get('product/duplicate/{product}', [ProductController::class, 'duplicate'])->name('product.duplicate');
 
 
 
